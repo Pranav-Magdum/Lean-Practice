758eb028b731d4516ee560f9cec452d094922678
Pranav-Magdum <pranavmagdum118@gmail.com>
2026-05-01 10:01:19 +0530

    Add files via upload

--- P1.lean
+++ P1.lean
@@ -1,1 +1,14 @@
-import P1.Basic
+namespace Pranav
+def dist(v t : Nat): Nat := v*t
+def mom(m v : Nat) : Nat := m*v
+
+theorem dist_zero_time (v : Nat) :
+dist v 0 = 0 := by simp[dist]
+
+theorem mom_zero_velocity (m : Nat) :
+mom m 0 = 0 := by simp[mom]
+
+theorem dist_comm (v t: Nat):
+dist v t = dist t v := by
+simp[dist]
+rw[Nat.mul_comm]
